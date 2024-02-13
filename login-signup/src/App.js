76e9aa203e625b4bbe78e095e699cd5be7e61cb9
Harsh Tyagi <tyagiharsh216@gmail.com>
2024-02-13 10:31:28 +0530

    Basic login and Signup Done

---
+++ login-signup/src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Navbar } from './Component/Navbar';
+import Login from './Component/Login';
+import Signup from './Component/Signup';
+
+
+
+function App() {
+  return (
+    <>
+    <div className="App">
+        <BrowserRouter>
+    <Navbar /> 
+    <div className="container">
+      <Routes>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+      </Routes>
+      </div> 
+    </BrowserRouter>
+    </div>
+    </>
+  );
+}
+
+export default App;
